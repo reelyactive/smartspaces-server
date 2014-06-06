64c3c23d50a56a312419cf9ad2f34cc9c7c75624
Marek <marekz@gmail.com>
2014-06-06 16:36:41 -0400

    Correct the code indentation.

--- server.js
+++ server.js
@@ -7,23 +7,22 @@ app.use(express.static('public'));
 
 // Request handlers
 app.get('/', function(req, res) {
+    // Redirect the root path to our default area.
     res.redirect(307, '/notman');
 });
 
+// List of acceptable pages on which to serve the web client.
 var pages = ['notman', 'greenhouse'];
 
 app.get('/:identifier', function(req, res) {
-	if (pages.indexOf(req.params.identifier) != -1) {
-		res.sendfile("public/client.html")
-	} else {
-		res.status(404).send('Not found');
-	}
+  if (pages.indexOf(req.params.identifier) != -1) {
+    res.sendfile("public/client.html")
+  } else {
+    res.status(404).send('Not found');
+  }
 });
-
-
- 
 
 var port = process.env.port || 3000;
 app.listen(port, function() {
-	console.log("smartpaces-server is listening on port", port);
+  console.log("smartpaces-server is listening on port", port);
 });
