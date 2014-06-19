1c99ef5e923ca892b39af6b24e8f4b60b0b0be5b
Marek <marekz@gmail.com>
2014-06-19 16:19:50 -0400

    Change static file serving and index file name.

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require("express");
 var app = express();
 
 
-// Static files directory
-app.use(express.static('public'));
-
 // Request handlers
 app.get('/', function(req, res) {
     // Redirect the root path to our default area.
@@ -16,11 +13,14 @@ var pages = ['notman', 'greenhouse'];
 
 app.get('/:identifier', function(req, res) {
   if (pages.indexOf(req.params.identifier) != -1) {
-    res.sendfile("public/client.html")
+    res.sendfile("public/index.html");
   } else {
     res.status(404).send('Not found');
   }
 });
+
+// Static files directory
+app.use(express.static('public'));
 
 var port = process.env.port || 3000;
 app.listen(port, function() {
