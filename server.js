24276f525c984edea9165ad21248c0f1b7d088d1
Marek <marekz@gmail.com>
2014-06-19 16:41:12 -0400

    Factor out the public directory

--- server.js
+++ server.js
@@ -2,25 +2,29 @@ var express = require("express");
 var app = express();
 
 
+// List of acceptable pages on which to serve the web client.
+var pages = ['notman', 'greenhouse'];
+
+// Directory containing the web client.
+var publicDir = 'public';
+
+
 // Request handlers
 app.get('/', function(req, res) {
     // Redirect the root path to our default area.
     res.redirect(307, '/notman');
 });
 
-// List of acceptable pages on which to serve the web client.
-var pages = ['notman', 'greenhouse'];
-
 app.get('/:identifier', function(req, res) {
   if (pages.indexOf(req.params.identifier) != -1) {
-    res.sendfile("public/index.html");
+    res.sendfile(publicDir + "/index.html");
   } else {
     res.status(404).send('Not found');
   }
 });
 
 // Static files directory
-app.use(express.static('public'));
+app.use(express.static(publicDir));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
